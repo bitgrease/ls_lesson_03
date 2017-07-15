d56ac38f9ca00b69a9a39720e3296aa83aa067eb
Kelly Netterville <bitgrease@gmail.com>
2017-07-15 19:05:03 -0400

    Exercise 6 from Medium1 questions.

---
+++ Med1Prob/m1_question6.rb
@@ -0,0 +1,16 @@
+# changed to return copies of the modified params and leave
+# originals intact and unchanged.
+
+def not_so_tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param += ["rutabaga"]
+
+  return a_string_param, an_array_param
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+my_string, my_array = not_so_tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
